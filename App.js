75bc2a39be754dffcc2e4871ed8b82469aa0d837
KimiRaykkony <raykkonyguilherme@gmail.com>
2024-03-31 00:24:09 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { styles } from './src/style';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>SPLASH!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
